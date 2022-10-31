01f94b9a062025ce0f5cc63bf588713c903e8fbe
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-30 21:14:15 -0300

    :test_tube: test: Product class repr method

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = {
+        "id": 1,
+        "nome_da_empresa": "Elma Chips",
+        "nome_do_produto": "Batata Frita Ondulada Original Ruffles 76g",
+        "data_de_fabricacao": "2022/10/30",
+        "data_de_validade": "2023/02/30",
+        "numero_de_serie": "1S26 2B4K 69WX UY57",
+        "instrucoes_de_armazenamento": "Guarde em local arejado.",
+    }
+
+    product = Product(**mock)
+
+    expected = (
+        f"O produto {mock['nome_do_produto']}"
+        f" fabricado em {mock['data_de_fabricacao']}"
+        f" por {mock['nome_da_empresa']} com validade"
+        f" até {mock['data_de_validade']}"
+        f" precisa ser armazenado {mock['instrucoes_de_armazenamento']}."
+    )
+
+    assert repr(product) == expected
