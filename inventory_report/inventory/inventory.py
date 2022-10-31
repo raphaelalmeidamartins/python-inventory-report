f2fe598ee82704ceee80d2d2778e38d6b7a93250
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-31 16:50:45 -0300

    :sparkles: feat: Invetory class

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,22 @@
+import csv
+
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, file_path: str, report_type: str):
+        with open(file_path, encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            docs_list = list(reader)
+
+        if report_type == "simples":
+            return SimpleReport.generate(docs_list)
+
+        if report_type == "completo":
+            return CompleteReport.generate(docs_list)
+
+        raise ValueError(
+            "Invalid report type, it needs to be 'simples' or 'completo'"
+        )
