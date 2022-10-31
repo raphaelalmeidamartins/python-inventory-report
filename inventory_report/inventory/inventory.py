0f80aa9a04d1f2b303708cb2fb92cc7160109acf
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-31 18:25:56 -0300

    :sparkles: feat: Inventory class can now read json and xml files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,7 @@
 import csv
+import json
+
+import xmltodict
 
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
@@ -8,8 +11,18 @@ class Inventory:
     @classmethod
     def import_data(cls, file_path, report_type):
         with open(file_path, encoding="utf-8") as file:
-            reader = csv.DictReader(file, delimiter=",", quotechar='"')
-            docs_list = list(reader)
+            file_extension = file_path.split(".")[-1]
+
+            if file_extension not in ["csv", "json", "xml"]:
+                raise ValueError("Invalid file extension")
+
+            read_methods = {
+                "csv": cls.__read_csv,
+                "json": cls.__read_json,
+                "xml": cls.__read_xml,
+            }
+
+            docs_list = read_methods[file_extension](file)
 
         if report_type == "simples":
             return SimpleReport.generate(docs_list)
@@ -20,3 +33,19 @@ class Inventory:
         raise ValueError(
             "Invalid report type, it needs to be 'simples' or 'completo'"
         )
+
+    @classmethod
+    def __read_csv(cls, file):
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        docs_list = list(reader)
+        return docs_list
+
+    @classmethod
+    def __read_json(cls, file):
+        docs_list = json.load(file)
+        return docs_list
+
+    @classmethod
+    def __read_xml(cls, file):
+        docs_list = xmltodict.parse(file.read())["dataset"]["record"]
+        return docs_list
