f903af95ffecc3879b17825ac34ee96f947da156
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-02 17:49:23 -0300

    :sparkles: feat: implement the main function

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,13 @@
+import sys
+
+from inventory_report.inventory.inventory import Inventory
+
+
 def main():
-    pass
+    try:
+        file_path, report_type = sys.argv[1:]
+        report = Inventory.import_data(file_path, report_type)
+
+        sys.stdout.write(report)
+    except (IndexError, KeyError, ValueError):
+        sys.stderr.write("Verifique os argumentos\n")
